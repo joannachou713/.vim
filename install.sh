fae3f6e2e08755de5be0045c3cfb46bc4085c4d8
Joanna Chien <joannachou713@gmail.com>
2021-10-24 15:21:28 +0800

    Add bash script to install all plugins(for pathogen)

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+mkdir bundle
+cd bundle
+# Installing Coc
+git clone https://github.com/neoclide/coc.nvim.git
+# to use Tabnine, run `:CocInstall coc-tabnine` inside your vim
+
+# Installing NERDCommenter
+git clone https://github.com/preservim/nerdcommenter.git
+
+# Installing Airline
+git clone https://github.com/vim-airline/vim-airline.git
+
+# Installing devicons
+git clone https://github.com/ryanoasis/vim-devicons.git
+
+# Installing horizon theme from vscode
+git clone https://github.com/ntk148v/vim-horizon.git
+
+# Installing ctrlp.vim
+git clone https://github.com/ctrlpvim/ctrlp.vim.git
+
+# Installing NERDTree
+git clone https://github.com/preservim/nerdtree.git
+
+# Installing Airline-themes
+git clone https://github.com/vim-airline/vim-airline-themes.git
+
+# Installing gitgutter
+git clone https://github.com/airblade/vim-gitgutter.git
+
+# Installing solarized8 theme
+git clone https://github.com/lifepillar/vim-solarized8.git
